be5cf077632b7b200659224b4deca65f18451a52
matowdev <79189126+matowdev@users.noreply.github.com>
2026-07-16 07:02:01 +0300

    [LES-2.2/ret] intro-to-n-current-record-mdle
    Edit comments.
    
    SS-for-dev

--- super-training/suitescript-for-dev/2-scripting/2-2-intro-to-n-current-record-module/memo_field_validate_checker_cs.js
+++ super-training/suitescript-for-dev/2-scripting/2-2-intro-to-n-current-record-module/memo_field_validate_checker_cs.js
@@ -1,6 +1,5 @@
 /**
  * @NApiVersion 2.1
- * @NModuleScope SameAccount
  * @NScriptType ClientScript
  *
  * Validates the "memo" field. Detects the invalid word "bad" and automatically replaces it with asterisks (***).
@@ -16,28 +15,28 @@ define([], () => {
     try {
       const { currentRecord, fieldId } = context;
 
-      // checking changes/entries only in the "memo" field
+      // check changes/entries only in the "memo" field
       if (fieldId === 'memo') {
         const badValue = 'bad';
-        const memoValue = currentRecord.getValue({ fieldId: 'memo' }) ?? ''; // getting the current/entered value (null/undefined exception)
-        const containsBadValue = memoValue.toLowerCase().includes(badValue); // checking for a "bad" value (true/false)
+        const memoValue = currentRecord.getValue({ fieldId: 'memo' }) ?? ''; // get the current/entered value (null/undefined exception)
+        const containsBadValue = memoValue.toLowerCase().includes(badValue); // check for a "bad" value (true/false)
 
         if (containsBadValue === true) {
-          // setting the intended value and ignoring repeated triggers
+          // set the intended value and ignore re-triggers
           currentRecord.setValue({
             fieldId: 'memo',
-            value: memoValue.replace(new RegExp(badValue, 'gi'), '***'), // replacing a “bad” value with the provided one "***" (i.e. through a regular expression from badValue, with flags "g" (global/all occurrences), "i" (case insensitive).. alternative to the replaceAll() method which may not be supported, skips cases)
-            ignoreFieldChange: true, // exclusion of re-validation (immediately) after replacing “bad” with “***” (i.e., in essence, exclusion of possible recursion)
+            value: memoValue.replace(new RegExp(badValue, 'gi'), '***'), // replace a "bad" value with the provided one "***" (i.e. through a regular expression from badValue, with flags "g" (global/all occurrences), "i" (case insensitive).. alternative to the replaceAll() method which may not be supported, skips cases)
+            ignoreFieldChange: true, // exclude re-validation (immediately) after replacing "bad" with "***" (i.e. in essence, exclude possible recursion)
           });
 
-          return false; // canceling the user's original input containing "bad" (i.e. "bad" will not be written, but "***" will be written)
+          return false; // cancel the user's original input containing "bad" (i.e. "bad" will not be written, but "***" will be written)
         }
       }
     } catch (e) {
       console.error(`${SCRIPT_FILENAME}: validateField error:`, e);
     }
 
-    return true; // after all.. return true, because we either/have already corrected the value, or there was no "bad" in the field (i.e. there is no barrier to other interactions)
+    return true; // after all.. return "true", because we either/have already corrected the value, or there was no "bad" in the field (i.e. there is no barrier to other interactions)
   };
 
   return {
